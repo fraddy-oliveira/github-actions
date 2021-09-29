8c935aef8b20f1a2aa9226f93b62ccbdbaae0ffe
Fraddy Oliveira <fraddy.dev@gmail.com>
2021-09-29 21:16:01 +0530

    Integrate eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2017: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+};
